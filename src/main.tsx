35f39cd1926f09b9c1b7a9b8d784d98311561e86
sonnyding1 <sonnyding20@gmail.com>
2024-03-24 23:41:52 -0700

    chore

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { NextUIProvider } from "@nextui-org/react";
-import App from "./App.tsx";
 import "./index.css";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Root from "./routes/root.tsx";

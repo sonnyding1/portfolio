1507443c630539022889a16879022a6627916c15
Sonny Ding <sonnyding20@gmail.com>
2024-04-02 16:32:16 -0700

    Remove posthog-js dependency and related code

--- src/main.tsx
+++ src/main.tsx
@@ -6,12 +6,6 @@ import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import Root from "./routes/root.tsx";
 import ErrorPage from "./routes/error-page.tsx";
 import Project, { loader as projectLoader } from "./routes/project.tsx";
-import posthog from "posthog-js";
-import { PostHogProvider } from "posthog-js/react";
-
-posthog.init("phc_npn686JeeLdaGoCHOdFvqNb289AFZk3W3T11ohrLgb5", {
-  api_host: "https://app.posthog.com",
-});
 
 const router = createBrowserRouter([
   {
@@ -29,11 +23,9 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <PostHogProvider client={posthog}>
-      <NextUIProvider>
-        {/* <App /> */}
-        <RouterProvider router={router} />
-      </NextUIProvider>
-    </PostHogProvider>
+    <NextUIProvider>
+      {/* <App /> */}
+      <RouterProvider router={router} />
+    </NextUIProvider>
   </React.StrictMode>
 );

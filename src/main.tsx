ecddb1ed79a1abac8d86fd3796ca65b1cc20300d
Sonny Ding <sonnyding20@gmail.com>
2024-04-02 16:16:29 -0700

    Add PostHogProvider to main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,7 @@ import Root from "./routes/root.tsx";
 import ErrorPage from "./routes/error-page.tsx";
 import Project, { loader as projectLoader } from "./routes/project.tsx";
 import posthog from "posthog-js";
+import { PostHogProvider } from "posthog-js/react";
 
 const posthogApiKey = process.env.REACT_APP_POSTHOG_API_KEY || ""; // Set a default value if the environment variable is undefined
 posthog.init(posthogApiKey, {
@@ -29,9 +30,11 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <NextUIProvider>
-      {/* <App /> */}
-      <RouterProvider router={router} />
-    </NextUIProvider>
+    <PostHogProvider client={posthog}>
+      <NextUIProvider>
+        {/* <App /> */}
+        <RouterProvider router={router} />
+      </NextUIProvider>
+    </PostHogProvider>
   </React.StrictMode>
 );
